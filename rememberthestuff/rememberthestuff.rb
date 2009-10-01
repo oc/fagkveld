7e7e487eef0f9d3768edb8a9932f3bfe6671e299
Ole Christian Rynning <oc@rynning.no>
2009-10-01 16:33:36 +0200

    Step 4 done

--- rememberthestuff/rememberthestuff.rb
+++ rememberthestuff/rememberthestuff.rb
@@ -1,6 +1,7 @@
 require 'rubygems' 
 require 'sinatra'
 require 'dm-core'
+require 'haml'
 require 'sass'
 
 # Setup DataMapper to use appengine datastore
@@ -11,7 +12,8 @@ class List
   property :id, Serial
   property :name, String
   property :due_at, DateTime
-  
+
+  has n, :items
 end
 
 get '/' do
@@ -24,6 +26,35 @@ post '/' do
   list = List.create(:name => params[:name],
     :due_at => DateTime.new(params[:year].to_i, params[:month].to_i, params[:day].to_i, params[:hour].to_i, params[:min].to_i))
   redirect '/'
+end
+
+class Item
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :list_id, Integer
+  property :description, Text
+  property :got_at, DateTime
+end
+
+get '/:list' do
+  @list = List.get(params[:list])
+  @needed_items = Item.all(:list_id => @list.id, :got_at => nil)
+  @got_items = Item.all(:list_id => @list.id).select {|it| it.got_at != nil }
+  erb :items
+end
+
+post '/:list' do
+  list = List.get(params[:list])
+  stuff = Item.create(:list_id => list.id, :description => params[:description])
+  redirect "/#{params[:list]}"
+end
+
+get '/:list/:item' do
+  item = Item.get(params[:item])
+  item.got_at = DateTime.now
+  item.save
+  redirect "/#{params[:list]}"
 end
 
 get '/stylesheet.css' do
